2c51e2a736b885e6d9174630f5b1115d0b82f45a
jack <473330930@qq.com>
2024-03-09 10:43:44 +0800

    logo修改

--- ruoyi-ui/src/layout/components/Sidebar/Logo.vue
+++ ruoyi-ui/src/layout/components/Sidebar/Logo.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import logoImg from '@/assets/logo/logo.png'
+import logoImg from '@/assets/logo/logo.jpg'
 import variables from '@/assets/styles/variables.scss'
 
 export default {
